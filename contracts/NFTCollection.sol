cccff957cd1c1a66966651906a9d52eaf4b6b191
JuanCommits <juanpedrodasilvabarloco@gmail.com>
2024-08-21 18:44:46 -0300

    Added collection creation

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -6,11 +6,9 @@ import "@openzeppelin/contracts@4.9.6/access/Ownable.sol";
 
 contract NFTCollection is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
-    string public baseTokenURI;
 
-    constructor(string memory name, string memory symbol, string memory baseURI) ERC721(name, symbol) {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
         tokenCounter = 0;
-        baseTokenURI = baseURI;
     }
 
     function createNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
@@ -19,9 +17,5 @@ contract NFTCollection is ERC721URIStorage, Ownable {
         _setTokenURI(newItemId, tokenURI);
         tokenCounter++;
         return newItemId;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI;
     }
 }
