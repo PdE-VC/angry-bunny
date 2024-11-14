1c31668d5e1489892334ca23a758c2619f4c4113
JuanCommits <juanpedrodasilvabarloco@gmail.com>
2024-11-14 11:09:29 -0300

    Added first version of web

---
+++ web/src/content/narrative-data.js
@@ -0,0 +1,101 @@
+export const allFragments = {
+    "Purpose":[{
+      index: "1",
+      name: "Short name",
+      vid: "img1",
+      desc: "We transform your ideas into tangible results. Our deliverables are meticulously crafted to exceed expectations, ensuring every project milestone is met with precision and excellence. From concept to completion."
+    },
+    {
+      index: "2",
+      desc: "Something",
+      vid: "img2",
+      desc: "Our brand and event design services create compelling visual identities that leave lasting impressions. We craft cohesive brand experiences and design engaging event spaces that tell your story, connect with your audience, and elevate your brand."
+    },
+    {
+      index: "3",
+      desc: "Akira",
+      vid: "img3",
+      desc: "Through expert videography and photography, we capture the essence of your brand. Our visual storytelling combines technical excellence with creative vision, delivering powerful imagery that resonates with your target audience."
+    },
+    {
+      index: "4",
+      desc: "Angryyy",
+      vid: "img4",
+      desc: "Our motion design expertise brings static concepts to life. We create dynamic visual experiences through animation, kinetic typography, and fluid transitions, ensuring your message not only reaches but captivates your audience in today's fast-paced landscape."
+    }],
+
+    "Merit": [{
+      index: "1",
+      name: "Short name",
+      vid: "img1",
+      desc: "We transform your ideas into tangible results. Our deliverables are meticulously crafted to exceed expectations, ensuring every project milestone is met with precision and excellence. From concept to completion."
+    },
+    {
+      index: "2",
+      name: "Something",
+      vid: "img2",
+      desc: "Our brand and event design services create compelling visual identities that leave lasting impressions. We craft cohesive brand experiences and design engaging event spaces that tell your story, connect with your audience, and elevate your brand."
+    },
+    {
+      index: "3",
+      name: "Akira",
+      vid: "img3",
+      desc: "Through expert videography and photography, we capture the essence of your brand. Our visual storytelling combines technical excellence with creative vision, delivering powerful imagery that resonates with your target audience."
+    },
+    {
+      index: "4",
+      name: "Angryyy",
+      vid: "img4",
+      desc: "Our motion design expertise brings static concepts to life. We create dynamic visual experiences through animation, kinetic typography, and fluid transitions, ensuring your message not only reaches but captivates your audience in today's fast-paced landscape."
+    }],
+
+    "Ritual": [{
+      index: "1",
+      name: "Short name",
+      vid: "img1",
+      desc: "We transform your ideas into tangible results. Our deliverables are meticulously crafted to exceed expectations, ensuring every project milestone is met with precision and excellence. From concept to completion."
+    },
+    {
+      index: "2",
+      name: "Something",
+      vid: "img2",
+      desc: "Our brand and event design services create compelling visual identities that leave lasting impressions. We craft cohesive brand experiences and design engaging event spaces that tell your story, connect with your audience, and elevate your brand."
+    },
+    {
+      index: "3",
+      name: "Akira",
+      vid: "img3",
+      desc: "Through expert videography and photography, we capture the essence of your brand. Our visual storytelling combines technical excellence with creative vision, delivering powerful imagery that resonates with your target audience."
+    },
+    {
+      index: "4",
+      name: "Angryyy",
+      vid: "img4",
+      desc: "Our motion design expertise brings static concepts to life. We create dynamic visual experiences through animation, kinetic typography, and fluid transitions, ensuring your message not only reaches but captivates your audience in today's fast-paced landscape."
+    }],
+
+    "Realignment": [{
+      index: "1",
+      name: "Short name",
+      vid: "img1",
+      desc: "We transform your ideas into tangible results. Our deliverables are meticulously crafted to exceed expectations, ensuring every project milestone is met with precision and excellence. From concept to completion."
+    },
+    {
+      index: "2",
+      name: "Something",
+      vid: "img2",
+      desc: "Our brand and event design services create compelling visual identities that leave lasting impressions. We craft cohesive brand experiences and design engaging event spaces that tell your story, connect with your audience, and elevate your brand."
+    },
+    {
+      index: "3",
+      name: "Akira",
+      vid: "img3",
+      desc: "Through expert videography and photography, we capture the essence of your brand. Our visual storytelling combines technical excellence with creative vision, delivering powerful imagery that resonates with your target audience"
+    },
+    {
+      index: "4",
+      name: "Angryyy",
+      vid: "img4",
+      desc: "Our motion design expertise brings static concepts to life. We create dynamic visual experiences through animation, kinetic typography, and fluid transitions, ensuring your message not only reaches but captivates your audience in today's fast-paced landscape."
+    }],
+}
